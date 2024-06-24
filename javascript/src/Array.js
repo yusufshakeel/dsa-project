abe3b5b99b87dff3fc48bb8944bd1bdd96e3bd40
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-06-24 20:46:48 +0530

    insert and its different variations

--- javascript/src/Array.js
+++ javascript/src/Array.js
@@ -9,11 +9,61 @@ class Arr {
         return this.tailIndex === this.lastArrayIndex;
     }
 
+    isEmpty() {
+        return this.tailIndex === -1;
+    }
+
+    getTailIndex() {
+        return this.tailIndex;
+    }
+
+    getRaw() {
+        // deep clone
+        return JSON.parse(JSON.stringify(this.arr));
+    }
+
     insert(value) {
         if(this.isFull()) {
             throw new Error('Array is full.');
         }
         this.arr[++this.tailIndex] = value;
+    }
+
+    insertAtBeginningWithoutRightShift(value) {
+        if(this.isEmpty()) {
+            this.arr[++this.tailIndex] = value;
+        } else {
+            this.arr[0] = value;
+        }
+    }
+    
+    insertAtBeginningWithRightShift(value) {
+        if (this.isEmpty()) {
+            this.arr[++this.tailIndex] = value;
+            return;
+        }
+
+        if (!this.isFull()) {
+            this.tailIndex++;
+        }
+
+        for (let i = this.tailIndex; i >= 1; i--) {
+            this.arr[i] = this.arr[i - 1];
+        }
+
+        this.arr[0] = value;
+    }
+
+    insertAtIndex(index, value) {
+        if (index < 0 || index > this.lastArrayIndex) {
+            throw new Error('Invalid index.');
+        }
+
+        this.arr[index] = value;
+
+        if (this.tailIndex < index) {
+            this.tailIndex = index;
+        }
     }
 
     traverse() {
--- javascript/src/Array.test.js
+++ javascript/src/Array.test.js
@@ -35,6 +35,114 @@ describe('Testing Array', () => {
         });
     });
 
+    describe('Testing insertAtBeginningWithoutRightShift', () => {
+        it('should be able to insert at 0th index when array is empty', () => {
+            const arr = new Arr(5);
+            expect(arr.isEmpty()).toBeTruthy();
+            arr.insertAtBeginningWithoutRightShift(10);
+            expect(arr.isEmpty()).toBeFalsy();
+            expect(arr.getTailIndex()).toBe(0);
+            expect(arr.getRaw()).toStrictEqual([10, null, null, null, null]);
+        });
+
+        it('should be able to insert at 0th index when array is not empty', () => {
+            const arr = new Arr(5);
+            arr.insert(10);
+            arr.insert(20);
+            arr.insert(30);
+            
+            expect(arr.isEmpty()).toBeFalsy();
+            expect(arr.getRaw()).toStrictEqual([10, 20, 30, null, null]);
+
+            arr.insertAtBeginningWithoutRightShift(40);
+
+            expect(arr.getRaw()).toStrictEqual([40, 20, 30, null, null]);
+        });
+    });
+
+    describe('Testing insertAtBeginningWithRightShift', () => {
+        it('should be able to insert at 0th index when array is empty', () => {
+            const arr = new Arr(3);
+
+            expect(arr.getTailIndex()).toBe(-1);
+            expect(arr.getRaw()).toStrictEqual([null, null, null]);
+
+            arr.insertAtBeginningWithRightShift(10);
+
+            expect(arr.getTailIndex()).toBe(0);
+            expect(arr.getRaw()).toStrictEqual([10, null, null]);
+        });
+
+        it('should be able to insert at 0th index when array is not empty', () => {
+            const arr = new Arr(3);
+            arr.insert(10);
+            arr.insert(20);
+
+            expect(arr.getTailIndex()).toBe(1);
+            expect(arr.getRaw()).toStrictEqual([10, 20, null]);
+
+            arr.insertAtBeginningWithRightShift(40);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([40, 10, 20]);
+        });
+
+        it('should be able to insert at 0th index when array is full empty', () => {
+            const arr = new Arr(3);
+            arr.insert(10);
+            arr.insert(20);
+            arr.insert(30);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([10, 20, 30]);
+
+            arr.insertAtBeginningWithRightShift(40);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([40, 10, 20]);
+        });
+    });
+
+    describe('Testing insertAtIndex', () => {
+        it('should throw an error when index is less than zero.', () => {
+            const arr = new Arr(3);
+            expect(() => arr.insertAtIndex(-100, 1)).toThrow('Invalid index.');
+        });
+
+        it('should throw an error when index is greater than the last index of the array', () => {
+            const arr = new Arr(3);
+            expect(() => arr.insertAtIndex(10, 1)).toThrow('Invalid index.');
+        });
+
+        it('should be able to insert at a given index', () => {
+            const arr = new Arr(3);
+            arr.insert(10);
+
+            expect(arr.getTailIndex()).toBe(0);
+            expect(arr.getRaw()).toStrictEqual([10, null, null]);
+
+            arr.insertAtIndex(2, 20);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([10, null, 20]);
+        });
+
+        it('should be able to insert at a give index even when the array is full', () => {
+            const arr = new Arr(3);
+            arr.insert(10);
+            arr.insert(20);
+            arr.insert(30);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([10, 20, 30]);
+
+            arr.insertAtIndex(1, 100);
+
+            expect(arr.getTailIndex()).toBe(2);
+            expect(arr.getRaw()).toStrictEqual([10, 100, 30]);
+        });
+    });
+
     describe('Testing traverse', () => {
         it('should be able to traverse the array', () => {
             const arr = new Arr(3);

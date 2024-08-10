fb96dda5e5f7aeaabe49b08ea3629e5b068e6425
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-08-10 11:19:48 +0530

    refactoring

--- javascript/src/sorting/BubbleSort.js
+++ javascript/src/sorting/BubbleSort.js
@@ -1,7 +1,7 @@
 class BubbleSort {
     sort(arr) {
-        for(let i = 0; i < arr.length; i++) {
-            for(let j = 0; j < arr.length - i - 1; j++) {
+        for(let i = 1; i < arr.length; i++) {
+            for(let j = 0; j < arr.length - i; j++) {
                 if(arr[j] > arr[j+1]) {
                     const temp = arr[j];
                     arr[j] = arr[j+1];

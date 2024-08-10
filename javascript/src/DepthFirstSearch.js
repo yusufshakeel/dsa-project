af4ad7242e96f6fa967b5ddbbe72d08080aa1322
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-08-10 10:35:06 +0530

    post order traversal

--- javascript/src/DepthFirstSearch.js
+++ javascript/src/DepthFirstSearch.js
@@ -13,6 +13,21 @@ class DepthFirstSearch {
         traverse(temp);
         return result;
     }
+
+    postOrderTraversal(binarySearchTree) {
+        let temp = binarySearchTree.getRoot();
+        if (temp === null) {
+            return [];
+        }
+        let result = [];
+        const traverse = (node) => {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            result.push(node.value);
+        };
+        traverse(temp);
+        return result;
+    }
 }
 
 module.exports = { DepthFirstSearch };
--- javascript/src/DepthFirstSearch.test.js
+++ javascript/src/DepthFirstSearch.test.js
@@ -25,4 +25,21 @@ describe('Depth First Search', () => {
             expect(dfs.preOrderTraverse(bst)).toStrictEqual([40,20,10,30,70,50,80]);
         });
     });
+
+    describe('Post order traversal', () => {
+        it('should return empty array if binary search tree is empty', () => {
+            expect(dfs.postOrderTraversal(bst)).toStrictEqual([]);
+        });
+
+        it('should return post order traversal result for non-empty binary search tree', () => {
+            bst.insert(40);
+            bst.insert(20);
+            bst.insert(70);
+            bst.insert(10);
+            bst.insert(30);
+            bst.insert(50);
+            bst.insert(80);
+            expect(dfs.postOrderTraversal(bst)).toStrictEqual([10,30,20,50,80,70,40]);
+        });
+    });
 });
